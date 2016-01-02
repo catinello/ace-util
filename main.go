7134b173ab07c20867332b2b99c9e67ea5b9b69e
Antonino Catinello <antonino@catinello.eu>
2016-01-02 13:38:52 +0100

    clean up

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const license = "MIT"
 var usage string = `ace - Command line utility for the Ace HTML template engine.
 
 Usage:
-  ace [-i | --inner=<FILE>] [-m | --map=<FILE>] [-s | --separator=<SYMBOL>] [-p | --stdout] [ -o | --output=<FILE>] <FILE>
+  ace [-i | --inner=<FILE>] [-m | --map=<FILE>] [-s | --separator=<SYMBOL>] [-p | --stdout] [-o | --output=<FILE>] <FILE>
   ace [-h | --help]
   ace [-v | --version]
 Options:
@@ -62,26 +62,18 @@ func main() {
 		inner = ""
 	}
 
-	// load, execute, generate ace templates and data
-	tpl, err := ace.Load(base, inner, nil)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(2)
-	}
-
-
 	if len(args["--output"].([]string)) > 0 {
 		output = args["--output"].([]string)[0]
 	} else {
 		output = path.Base(base) + ".html"
 	}
 
-	w, err := os.OpenFile(output, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0655)
+	// load, execute, generate ace templates and data
+	tpl, err := ace.Load(base, inner, nil)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(2)
 	}
-	defer w.Close()
 
 	var data map[string]interface{}
 
@@ -97,6 +89,13 @@ func main() {
 			os.Exit(2)
 		}
 	} else {
+		w, err := os.OpenFile(output, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0655)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(2)
+		}
+		defer w.Close()
+
 		if err := tpl.Execute(os.NewFile(w.Fd(), output), data); err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(2)
@@ -105,6 +104,10 @@ func main() {
 
 }
 
+// FileToMap opens the fileName and parses the content per line
+// and separates key/values by the given unicode separator
+// to return a map with the content. Keys are of string and values
+// are considered string or []string.
 func FileToMap(fileName, separator string) map[string]interface{} {
 	// hash table variable
 	var data map[string]interface{}
@@ -117,16 +120,19 @@ func FileToMap(fileName, separator string) map[string]interface{} {
 	}
 	defer file.Close()
 
+	// scan through file
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
+
 		// is the line long enough to be considered?
 		if len(line) < len(separator)+2 {
 			continue
-			// is the string a slice of strings []string?
 		} else if strings.Contains(line, separator) {
 			parts := strings.Split(line, separator)
+
+			// is the string a slice of strings []string?
 			if strings.Contains(parts[1], "[]string") {
 				slice := strings.Split(parts[1], "[]string{")
 				collection := strings.TrimSuffix(slice[1], "}")
@@ -137,6 +143,7 @@ func FileToMap(fileName, separator string) map[string]interface{} {
 					data[parts[0]] = append(data[parts[0]].([]string), strings.TrimPrefix(strings.TrimSuffix(strings.TrimSpace(v), "\""), "\""))
 				}
 			} else {
+				// it is a string
 				data[parts[0]] = parts[1]
 			}
 		}
@@ -146,9 +153,6 @@ func FileToMap(fileName, separator string) map[string]interface{} {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(3)
 	}
-
-	//	fmt.Println(data["Msgs"].([]string)[1])
-	//	fmt.Println(data)
 
 	return data
 }
